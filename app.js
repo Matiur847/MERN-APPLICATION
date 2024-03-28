3c43b7889cdb93535b6ffe28f7e1031a8f88659d
Matiur847 <107800262+Matiur847@users.noreply.github.com>
2024-03-28 21:05:07 +0600

    final code

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use("/api/v1", userRoute);
 app.use("/api/v1", orderRoute);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname + "/frontend/build")));
+app.use(express.static(path.join(__dirname + "/client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
 });
 
 app.use((req, res) => {
